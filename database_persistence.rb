b10968eb9c9aed27f750a60d9c2a77f53ecb0491
Tim Cummings <timjc86@gmail.com>
2020-08-19 21:58:14 -0500

    refactor: delegate appropriate tasks (like counting) to the database

--- database_persistence.rb
+++ database_persistence.rb
@@ -16,19 +16,34 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = 'SELECT * FROM lists WHERE id = $1;'
+    sql = <<~SQL
+      SELECT lists.*,
+       COUNT(todos.id) AS todos_count,
+       COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+       WHERE lists.id = $1
+       GROUP BY lists.id
+       ORDER BY lists.name;
+    SQL
     result = query(sql, id)
-    tuple = result.first
-    { id: tuple['id'], name: tuple['name'], todos: find_todos_for_list(tuple['id']) }
+
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = 'SELECT * FROM lists;'
+    sql = <<~SQL
+      SELECT lists.*,
+       COUNT(todos.id) AS todos_count,
+       COUNT(NULLIF(todos.completed, true)) AS todos_remaining_count
+        FROM lists
+        LEFT JOIN todos ON todos.list_id = lists.id
+       GROUP BY lists.id
+       ORDER BY lists.name;
+    SQL
     result = query(sql)
 
-    result.map do |tuple|
-      { id: tuple['id'], name: tuple['name'], todos: find_todos_for_list(tuple['id']) }
-    end
+    result.map { |tuple| tuple_to_list_hash(tuple) }
   end
 
   def create_new_list(name)
@@ -47,6 +62,14 @@ class DatabasePersistence
 
   def update_list_name(id, name)
     query 'UPDATE lists SET name = $1 WHERE id = $2;', name, id
+  end
+
+  def find_todos_for_list(list_id)
+    sql = 'SELECT * FROM todos WHERE list_id = $1;'
+    result = query(sql, list_id)
+    result.map do |tuple|
+      { id: tuple['id'], name: tuple['name'], completed: to_bool(tuple['completed']) }
+    end
   end
 
   def create_new_todo(list_id, todo_name)
@@ -80,11 +103,12 @@ class DatabasePersistence
     end
   end
 
-  def find_todos_for_list(list_id)
-    sql = 'SELECT * FROM todos WHERE list_id = $1;'
-    result = query(sql, list_id)
-    result.map do |tuple|
-      { id: tuple['id'], name: tuple['name'], completed: to_bool(tuple['completed']) }
-    end
+  def tuple_to_list_hash(tuple)
+    {
+      id: tuple['id'].to_i,
+      name: tuple['name'],
+      todos_count: tuple['todos_count'].to_i,
+      todos_remaining_count: tuple['todos_remaining_count'].to_i
+    }
   end
 end
